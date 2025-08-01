7d9f7eb929683958e6a7af6879fdc53dac68937a
dan <md.erhenede@gmail.com>
2025-08-01 17:21:25 +0100

    2nd commit

--- src/components/PageComponents/HomeComponents/Header.tsx
+++ src/components/PageComponents/HomeComponents/Header.tsx
@@ -1,7 +1,7 @@
 import { AddFiles } from "@/components/localComponents/AddFiles";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useUser } from "@clerk/nextjs";
+import { UserButton, useUser } from "@clerk/nextjs";
 import { Search } from "lucide-react";
 import React from "react";
 
@@ -11,7 +11,9 @@ export default function Header() {
   return (
     <header className="z-10 flex flex-col items-center justify-center gap-8">
       <div className="md:w-[40%]  flex flex-col justify-center gap-3">
-        <p className="text-center">Hi, {user?.fullName}</p>
+        <p className="flex justify-center gap-2 text-center">
+          <UserButton /> Hi, {user?.fullName}
+        </p>
         <p className="text-2xl font-bold text-center gradient-text">
           Welcome to the Daniel Recruiting Dashboard
         </p>
